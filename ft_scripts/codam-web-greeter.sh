66571f638ed47a32be3c261191bd5a6b4849dbf8
ibertran <128724498+ibtrd@users.noreply.github.com>
2024-09-20 10:14:05 +0200

    fix: nvm install scrit exits if $NVM_DIR exists

--- ft_scripts/codam-web-greeter.sh
+++ ft_scripts/codam-web-greeter.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 MY_AVATAR=$HOME/Pictures/kermit.webp
-MY_SCREENSAVER=$HOME/Pictures/marvinette.png
+MY_WALLPAPER=$HOME/Pictures/black_background.png
 
 AVATAR_PATH="/tmp/codam-web-greeter-user-avatar"
 WALLPAPER_PATH="/tmp/codam-web-greeter-user-wallpaper"
 
 cp $MY_AVATAR $AVATAR_PATH
-cp $MY_SCREENSAVER $WALLPAPER_PATH
+cp $MY_WALLPAPER $WALLPAPER_PATH
 
 chmod 666 $WALLPAPER_PATH $AVATAR_PATH
